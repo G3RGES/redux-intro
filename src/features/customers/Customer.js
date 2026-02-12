708f7f1dbd364451b8356e68ed11cd36487d4b11
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-12 11:17:59 +0200

    Update Customer.js

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function Customer() {
   const customer = useSelector((state) => state.customer.fullName);
-  console.log(customer);
+  // console.log(customer);
   return <h2>👋 Welcome, {customer}</h2>;
 }
 
