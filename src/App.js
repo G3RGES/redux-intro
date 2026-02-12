c4a7fc5408962ebf454c4c5e65a7f81360e6b5b6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-12 10:42:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
+  console.log(fullName);
+
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (
+        <>
+          <h2>👋 Welcome to the bank</h2>
+          <p>Sign up to open a new account</p>
+          <CreateCustomer />
+        </>
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
