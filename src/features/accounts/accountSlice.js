cb0a7647b5427055874f1196944838db7c633c77
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-13 10:43:53 +0200

    Update accountSlice.js

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,6 +2,7 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
@@ -10,6 +11,7 @@ export default function accountReducer(state = initialStateAccount, action) {
       return {
         ...state,
         balance: state.balance + action.payload,
+        isLoading: false,
       };
     case "account/withdraw":
       return {
@@ -34,55 +36,16 @@ export default function accountReducer(state = initialStateAccount, action) {
         balance: state.balance - state.loan,
       };
 
-    //* INCASE THE payLoan CAN BE PAID IN AMOUNTS IT TAKES A PAYLOAD WITH THE AMOUNT
-    //* PAID AND IT'S DEDUCTED FROM THE BALANCE AND THE LOAN IS SET TO
-    //* LOAN AMOUNT - AMOUNT PAID (state.loan - action.payload)
-    // case "account/payLoan":
-    //   return {
-    //     ...state,
-    //     loan: state.loan - action.payload,
-    //     loanPurpose: "",
-    //     balance: state.balance - action.payload,
-    //   };
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
 
     default:
       return state;
   }
 }
-
-// store.dispatch({
-//   type: "account/deposit",
-//   payload: 500,
-// });
-
-// store.dispatch({
-//   type: "account/withdraw",
-//   payload: 200,
-// });
-// console.log(store.getState());
-
-// store.dispatch({
-//   type: "account/requestLoan",
-//   payload: {
-//     amount: 1000,
-//     purpose: "car",
-//   },
-// });
-
-// console.log(store.getState());
-
-//*DISPATCH PAY LOAN WITH A PAYLOAD FOR AN AMOUNT
-// store.dispatch({
-//   type: "account/payLoan",
-//   payload: 500,
-// });
-
-//* DISPATCH PAY LOAN WITHOUT A PAYLOAD (LOAN PAID IN FULL ON DISPATCH)
-// store.dispatch({
-//   type: "account/payLoan",
-// });
-
-// console.log(store.getState());
 
 export function deposit(amount, currency) {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
